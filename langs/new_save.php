623159215971c07c09a4f73273dc1d7a4f17efc8
likux35 <starkus.lukas@inbox.lt>
2016-03-12 15:17:36 +0200

    Added files via upload

---
+++ langs/new_save.php
@@ -0,0 +1,14 @@
+<?php
+$langSource = $_POST['source'];
+if(isset($_POST["updateCode"]) AND strlen($_POST["updateCode"]) >3){
+	$s 	=	$_POST["updateCode"];
+} else {
+	$s = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 5);
+
+}
+
+$file = fopen("private/".$s.".json", "w");
+fwrite($file, $langSource);
+fclose($file);
+echo $s;
+?>
