20b808a4736589470147c2c294a02caef2614e4e
likux35 <starkus.lukas@inbox.lt>
2016-06-30 01:06:44 +0300

    Update and rename boss.js to bosas.js

--- bosas.js
+++ bosas.js
@@ -1,32 +1,30 @@
 API.on(API.CHAT, bobas);
 
 function bobas(data){
-	
-	var role = API.getUser(data.cid).permission;
 
 	if(data.message.indexOf("!bosas") > -1)
 	{
-		if(role == API.ROLE.HOST)
+		if(API.ROLE == 5)
                 {
 			API.sendChat("@"+data.un+" Jo , bosas su hosto staffu :)");
 		} 
-                else if(role == API.ROLE.COHOST)
+                else if(API.ROLE == 4)
                 {
 			API.sendChat("@"+data.un+" Jo , bosas su co-host staffu :)");
 		}
-		else if(role == API.ROLE.MANAGER)
+		else if(API.ROLE == 3)
                 {
 			API.sendChat("@"+data.un+" Jo , bosas su manager staffu :)");
 		}
-		else if(role == API.ROLE.BOUNCER)
+		else if(API.ROLE == 2)
                 {
 			API.sendChat("@"+data.un+" Jo , bosas su bouncer staffu :)");
 		}
-		else if(role == API.ROLE.DJ)
+		else if(API.ROLE == 1)
 		{
 			API.sendChat("@"+data.un+" Jo, bosas bet dar tik prdedantysis su rdj :)")
 		}
-		else if(role == API.ROLE.NONE)
+		else if(API.ROLE == 0)
 		{
 			API.sendChat("@"+data.un+" Deja, staffo neturi, bet esi pats sau bosas :D")
 		}
